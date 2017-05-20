531b700ec619e1d1cc7c4aeac7233e4771eb390e
Abishak Kodi <abishak.kodi@gmail.com>
2017-05-20 12:53:32 -0700

    Implemented Filter Tests and Filter Function

--- src/underbar.js
+++ src/underbar.js
@@ -99,6 +99,7 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter()
   };
 
   // Produce a duplicate-free version of the array.

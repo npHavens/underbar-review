ed65db147bc9d033021d00eb2f8cc22032d3fea5
Abishak Kodi <abishak.kodi@gmail.com>
2017-05-20 12:50:39 -0700

    Fixed Spec files for Filter and Index

--- src/underbar.js
+++ src/underbar.js
@@ -86,6 +86,13 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var filtered = [];
+    _.each(collection, function(element) {
+      if (test(element)) {
+        filtered.push(element);
+      }
+    })
+    return filtered;
   };
 
   // Return all elements of an array that don't pass a truth test.

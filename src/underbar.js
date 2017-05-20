5dfc785bc5e7ad3d796adf18d0b65c431716da59
Abishak Kodi <abishak.kodi@gmail.com>
2017-05-20 13:29:41 -0700

    Started reduce - started it

--- src/underbar.js
+++ src/underbar.js
@@ -170,6 +170,11 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    accumulator = accumulator || collection[0];
+    _.each(collection, function(element) {
+      accumulator = iterator(accumulator, element);
+      return accumulator;
+    })
   };
 
   // Determine if the array or object contains a given value (using `===`).

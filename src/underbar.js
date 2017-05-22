390d836e65b5b1f7ca7002bec0d00a146a3eb2ce
Nick Havens <npahavens@gmail.com>
2017-05-21 20:49:55 -0500

    Completed implementation of _.invoke

--- src/underbar.js
+++ src/underbar.js
@@ -400,6 +400,12 @@
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    return _.map(collection, function(element) {
+      if (typeof functionOrKey === 'function') {
+        return functionOrKey.apply(element, args);
+      }
+      return element[functionOrKey].apply(element, args);
+    })
   };
 
   // Sort the object's values by a criterion produced by an iterator.

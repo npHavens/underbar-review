3d4f9d355429706fe260ddc4be33d529e6e1b0ed
Nick Havens <npahavens@gmail.com>
2017-05-22 15:53:38 -0500

    refactored _.invoke to use arrow functions and rest parameter

--- src/underbar.js
+++ src/underbar.js
@@ -356,8 +356,8 @@
 
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
-  _.invoke = function(collection, functionOrKey, args) {
-    return _.map(collection, function(element) {
+  _.invoke = (collection, functionOrKey, ...args) => {
+    return _.map(collection, element => {
       if (typeof functionOrKey === 'function') {
         return functionOrKey.apply(element, args);
       }
